b34314be97a1a2dd3f95a47509d147cf89f533cb
KianDoyle <kianrk@icloud.com>
2025-06-10 01:00:31 +0100

    Update: updated SimpleRestController and SimpleService to InvoiceRestController and InvoiceService.

---
+++ src/main/java/com/kd/yazpaints/services/InvoiceService.java
@@ -0,0 +1,11 @@
+package com.kd.yazpaints.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InvoiceService {
+
+    public String generateInvoice(String invoiceJson) {
+        return null;
+    }
+}
