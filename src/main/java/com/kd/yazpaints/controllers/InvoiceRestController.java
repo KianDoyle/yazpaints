85ec0fae65d37ec30bdc1dbc279845a9921ec0b4
KianDoyle <kianrk@icloud.com>
2025-07-21 12:17:20 +0100

    Migration: migrated workflow to VSCode. Update: updated .gitignore to exclude sql session files form tracking.

--- src/main/java/com/kd/yazpaints/controllers/InvoiceRestController.java
+++ src/main/java/com/kd/yazpaints/controllers/InvoiceRestController.java
@@ -2,17 +2,21 @@ package com.kd.yazpaints.controllers;
 
 import com.kd.yazpaints.services.InvoiceService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/invoice")
+@RestController
+@RequestMapping("/invoice")
 public class InvoiceRestController {
 
     private final InvoiceService invoiceService;
 
     public InvoiceRestController(InvoiceService invoiceService) {
         this.invoiceService = invoiceService;
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> testController() {
+        return ResponseEntity.ok("You've reached the invoices endpoint!");
     }
 
     @PostMapping("/generate")
